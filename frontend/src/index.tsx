8c173afc9039dd361b803811b7cdb495779f2ebe
albertinesilva-42 <albertinesilva_@hotmail.com>
2022-05-19 00:05:28 -0300

    Navbar

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+ document.getElementById('root')
+);
 
-const root = ReactDOM.createRoot(
+/* const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+);*/
 
 
