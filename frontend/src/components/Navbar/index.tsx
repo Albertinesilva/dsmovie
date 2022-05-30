be4bd52d5a0315bcef8dcbe05d265f4d88b66b28
albertinesilva-42 <albertinesilva_@hotmail.com>
2022-05-30 18:30:16 -0300

    Camera.svg

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { ReactComponent as FilmeCartazIcon } from 'assets/img/archive-fill.svg';
+import { ReactComponent as FilmeCartazIcon } from 'assets/img/camera.svg';
 import './styles.css';
 
 function Navbar() {
